b18053c4959e325312cf17cd41bd1d6b724d826f
Christos Psarras <psarras@aices.rwth-aachen.de>
2020-10-25 17:33:50 +0100

    Fix C experiments

--- scripts/cluster/c.sh
+++ scripts/cluster/c.sh
@@ -1,56 +1,51 @@
 #!/bin/bash
 
-module purge
-module load DEVELOP
-module load intel/19.0
-module load gcc/8
-
-source ${HOME}/.zshrc.local
+source ${HOME}/.zshrc.lamp
 cd ${LAMP_EXPERIMENTS}/c
 
 export MKL_NUM_THREADS=1
 export OMP_NUM_THREADS=1
-export output=results/c.txt
+export LAMP_C_OUTPUT_DIR=${LAMP_OUTPUT_DIR}/c.txt
 
 make clean
 make all -j 10
 
-echo "algorithm;m;k;n;C;cs_time" > ${output}
+echo "algorithm;m;k;n;C;cs_time"                    > ${LAMP_C_OUTPUT_DIR}
 export LAMP_m=3000
 export LAMP_k=550
 export LAMP_n=3000
-./bin/gemm.x       $LAMP_m $LAMP_k $LAMP_n >> ${output}
-./bin/gemm.x       $LAMP_n $LAMP_n $LAMP_n large >> ${output}
-./bin/gemm_noup.x  $LAMP_m $LAMP_k $LAMP_n >> ${output}
+./bin/gemm.x       $LAMP_m $LAMP_k $LAMP_n          >> ${LAMP_C_OUTPUT_DIR}
+./bin/gemm.x       $LAMP_n $LAMP_n $LAMP_n large    >> ${LAMP_C_OUTPUT_DIR}
+./bin/gemm_noup.x  $LAMP_m $LAMP_k $LAMP_n          >> ${LAMP_C_OUTPUT_DIR}
 
-./bin/syrk.x       $LAMP_n $LAMP_k >> ${output}
-./bin/syrk_noup.x  $LAMP_n $LAMP_k >> ${output}
+./bin/syrk.x       $LAMP_n $LAMP_k                  >> ${LAMP_C_OUTPUT_DIR}
+./bin/syrk_noup.x  $LAMP_n $LAMP_k                  >> ${LAMP_C_OUTPUT_DIR}
 
-./bin/syr2k.x      $LAMP_n $LAMP_k >> ${output}
-./bin/syr2k_noup.x $LAMP_n $LAMP_k >> ${output}
+./bin/syr2k.x      $LAMP_n $LAMP_k                  >> ${LAMP_C_OUTPUT_DIR}
+./bin/syr2k_noup.x $LAMP_n $LAMP_k                  >> ${LAMP_C_OUTPUT_DIR}
 
-./bin/trmm.x       $LAMP_m $LAMP_n >> ${output}
-./bin/diagmm.x     $LAMP_n >> ${output}
+./bin/trmm.x       $LAMP_m $LAMP_n                  >> ${LAMP_C_OUTPUT_DIR}
+./bin/diagmm.x     $LAMP_n                          >> ${LAMP_C_OUTPUT_DIR}
 
-./bin/subexpr_nai.x $LAMP_m $LAMP_k >> ${output}
-./bin/subexpr_rec.x $LAMP_m $LAMP_k >> ${output}
+./bin/subexpr_nai.x $LAMP_m $LAMP_k                 >> ${LAMP_C_OUTPUT_DIR}
+./bin/subexpr_rec.x $LAMP_m $LAMP_k                 >> ${LAMP_C_OUTPUT_DIR}
 
-./bin/mc_mixed.x $LAMP_m $LAMP_k $LAMP_n >> ${output}
+./bin/mc_mixed.x $LAMP_m $LAMP_k $LAMP_n            >> ${LAMP_C_OUTPUT_DIR}
 
 export LAMP_tr_m=80
 export LAMP_tr_k=80
 export LAMP_tr_n=80
-./bin/transposition.x $LAMP_tr_m $LAMP_tr_k $LAMP_tr_n >> ${output}
+./bin/transposition.x $LAMP_tr_m $LAMP_tr_k $LAMP_tr_n >> ${LAMP_C_OUTPUT_DIR}
 
 export LAMP_rhs_small=100
-./bin/solve_naive.x       $LAMP_n $LAMP_rhs_small >> ${output}
-./bin/solve_recommended.x $LAMP_n $LAMP_rhs_small >> ${output}
+./bin/solve_naive.x       $LAMP_n $LAMP_rhs_small      >> ${LAMP_C_OUTPUT_DIR}
+./bin/solve_recommended.x $LAMP_n $LAMP_rhs_small      >> ${LAMP_C_OUTPUT_DIR}
 
-./bin/solve_recommended_spd.x $LAMP_n $LAMP_rhs_small >> ${output}
-./bin/solve_recommended_sym.x $LAMP_n $LAMP_rhs_small >> ${output}
-./bin/solve_recommended_tri.x $LAMP_n $LAMP_rhs_small >> ${output}
-./bin/solve_recommended_dia.x $LAMP_n $LAMP_rhs_small >> ${output}
+./bin/solve_recommended_spd.x $LAMP_n $LAMP_rhs_small  >> ${LAMP_C_OUTPUT_DIR}
+./bin/solve_recommended_sym.x $LAMP_n $LAMP_rhs_small  >> ${LAMP_C_OUTPUT_DIR}
+./bin/solve_recommended_tri.x $LAMP_n $LAMP_rhs_small  >> ${LAMP_C_OUTPUT_DIR}
+./bin/solve_recommended_dia.x $LAMP_n $LAMP_rhs_small  >> ${LAMP_C_OUTPUT_DIR}
 
 export LAMP_rhs_large=6000
-./bin/solve_naive.x       $LAMP_n $LAMP_rhs_large large >> ${output}
-./bin/solve_recommended.x $LAMP_n $LAMP_rhs_large large >> ${output}
+./bin/solve_naive.x       $LAMP_n $LAMP_rhs_large large >> ${LAMP_C_OUTPUT_DIR}
+./bin/solve_recommended.x $LAMP_n $LAMP_rhs_large large >> ${LAMP_C_OUTPUT_DIR}
