36f9cfea0e16ce462548f2914fde2b3e6a991c0f
Christos Psarras <psarras@aices.rwth-aachen.de>
2020-10-25 17:09:04 +0100

    Change script naming

---
+++ scripts/cluster/sub.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source $HOME/.zshrc.lamp
+
+export LAMP_LOG_DIR=${LAMP_EXPERIMENTS}/scripts/cluster/log
+
+#sbatch -J Matlab -o ${LAMP_LOG_DIR}/matlab.log    -N 1 -n 1 --mem=5000 -t 02:00:00 --partition=ih -A aices2 -C hwx2680 --nodelist=linuxihdc072 --exclusive ${LAMP_EXPERIMENTS}/scripts/cluster/matlab.sh
+#sbatch -J Octave -o ${LAMP_LOG_DIR}/octave.log    -N 1 -n 1 --mem=5000 -t 02:00:00 --partition=ih -A aices2 -C hwx2680 --nodelist=linuxihdc072 --exclusive ${LAMP_EXPERIMENTS}/scripts/cluster/octave.sh
+sbatch -J Julia  -o ${LAMP_LOG_DIR}/julia.log     -N 1 -n 1 --mem=5000 -t 02:00:00 --partition=ih -A aices2 -C hwx2680 --nodelist=linuxihdc072 --exclusive ${LAMP_EXPERIMENTS}/scripts/cluster/aices/aices_julia
+#sbatch -J C      -o ${LAMP_LOG_DIR}/c.log         -N 1 -n 1 --mem=5000 -t 02:00:00 --partition=ih -A aices2 -C hwx2680 --nodelist=linuxihdc072 --exclusive ${LAMP_EXPERIMENTS}/scripts/cluster/c.sh
+#sbatch -J CppArm -o ${LAMP_LOG_DIR}/cpp_armad.log -N 1 -n 1 --mem=5000 -t 02:00:00 --partition=ih -A aices2 -C hwx2680 --nodelist=linuxihdc072 --exclusive ${LAMP_EXPERIMENTS}/scripts/cluster/cpp_arma.sh
+#sbatch -J CppEig -o ${LAMP_LOG_DIR}/cpp_eigen.log -N 1 -n 1 --mem=5000 -t 02:00:00 --partition=ih -A aices2 -C hwx2680 --nodelist=linuxihdc072 --exclusive ${LAMP_EXPERIMENTS}/scripts/cluster/cpp_eig.sh
+#sbatch -J Python -o ${LAMP_LOG_DIR}/python.log    -N 1 -n 1 --mem=5000 -t 02:00:00 --partition=ih -A aices2 -C hwx2680 --nodelist=linuxihdc072 --exclusive ${LAMP_EXPERIMENTS}/scripts/cluster/python.sh
+#sbatch -J R      -o ${LAMP_LOG_DIR}/R.log         -N 1 -n 1 --mem=5000 -t 02:00:00 --partition=ih -A aices2 -C hwx2680 --nodelist=linuxihdc072 --exclusive ${LAMP_EXPERIMENTS}/scripts/cluster/r.sh
