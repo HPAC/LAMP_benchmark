23170e23adb68d35cca08570303d3b7c0fa51803
Christos Psarras <psarras@aices.rwth-aachen.de>
2020-11-25 14:06:51 +0100

    Make matlab and octave sparse identical

--- octave/properties_solve_sparse.m
+++ octave/properties_solve_sparse.m
@@ -11,21 +11,21 @@ function properties_solve_sparse(n, rhs, density, b)
   b.benchmark('solve_sp_gen', reps, custom_solve_);
 
   % Symmetric
-  A = sprandn(n,n,density/2);
+  A = sprandn(n,n,density/2.0);
   A = A + A' + 2.0 * speye(n,n);
   fprintf("Cond number: %.1f, nnz: %d\n", condest(A), nnz(A))
   custom_solve_ = @() custom_solve(A, B);
   b.benchmark('solve_sp_sym', reps, custom_solve_);
 
   % SPD
-  A = sprandn(n,n,density/2);
-  A = A + A' + speye(n,n)*n;
+  A = sprandn(n,n,density/2.0);
+  A = A + A' + n * speye(n,n);
   fprintf("Cond number: %.1f, nnz: %d\n", condest(A), nnz(A))
   custom_solve_ = @() custom_solve(A, B);
   b.benchmark('solve_sp_spd', reps, custom_solve_);
 
   % Triangular
-  A = sprandn(n,n,2*density);
+  A = sprandn(n,n,2.0*density);
   A = tril(A) + speye(n,n);
   fprintf("Cond number: %.1f, nnz: %d\n", condest(A), nnz(A))
   custom_solve_ = @() custom_solve(A, B);
