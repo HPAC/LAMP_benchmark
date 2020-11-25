d0e55d82ae69e36f928dca1524265856819d93f6
Christos Psarras <psarras@aices.rwth-aachen.de>
2020-11-25 14:18:29 +0100

    Fix Julia Sym outlier

--- julia/properties_solve_sparse.jl
+++ julia/properties_solve_sparse.jl
@@ -1,3 +1,5 @@
+using Printf
+
 function properties_solve_sparse(n, density)
 
   reps = parse(Int64, ENV["LAMP_REPS"])
@@ -7,20 +9,22 @@ function properties_solve_sparse(n, density)
   # Generic
   A = sprandn(n, n, density)
   A = A + sparse(I, n, n)
-  @test !issymmetric(A)
-  @test !isposdef(A)
+  Printf.@printf("cond: %.1f, nnz: %d\n", cond(A,1), nnz(A))
+  @test !issymmetric(A) && !isposdef(A)
   Benchmarker.add_data(csv, "solve_sp_gen", Benchmarker.measure(reps, solve, A, B))
   @test !isposdef(A) && !issymmetric(A)
 
   # Symmetric
-  A = sprandn(n, n, density)
+  A = sprandn(n, n, density/2.0)
   A = A + A' + 2.0 * sparse(I, n, n)
+  Printf.@printf("cond: %.1f, nnz: %d\n", cond(A,1), nnz(A))
   Benchmarker.add_data(csv, "solve_sp_sym", Benchmarker.measure(reps, solve, A, B))
   @test issymmetric(A) && !isposdef(A)
 
   # SPD
-  A = sprandn(n, n, density/2)
+  A = sprandn(n, n, density/2.0)
   A = A + A' + n * sparse(I, n, n)
+  Printf.@printf("cond: %.1f, nnz: %d\n", cond(A,1), nnz(A))
   @test issymmetric(A)
   @test isposdef(A)
   Benchmarker.add_data(csv, "solve_sp_spd", Benchmarker.measure(reps, solve, A, B))
@@ -29,6 +33,7 @@ function properties_solve_sparse(n, density)
   # Triangular
   A = sprandn(n, n, 2.0 * density)
   A = sparse(LowerTriangular(A)) + sparse(I,n,n)
+  Printf.@printf("cond: %.1f, nnz: %d\n", cond(A,1), nnz(A))
   Benchmarker.add_data(csv, "solve_sp_tri", Benchmarker.measure(reps, solve, A, B))
   @test istril(A)
 end
