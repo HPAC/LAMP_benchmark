0064514459e550d21d54f190ddf08e3d00aa1a41
Christos Psarras <psarras@aices.rwth-aachen.de>
2020-11-26 11:07:28 +0100

    Fix sparse experiments in R

--- r/properties_solve_sparse.R
+++ r/properties_solve_sparse.R
@@ -1,8 +1,37 @@
 library("sets")
 library(Matrix)
 
-solve_b <- function(A, B){
+solve_b <- function(n, rhs, type) {
 
+    if ("gen" %in% type) {
+      A <- rsparsematrix(n, n, density=density, rand.x=rnorm)
+      diag(A) <- diag(A) + 1.0
+      print("gen")
+    }
+    else if  ("sym" %in% type) {
+      A <- rsparsematrix(nrow=n, ncol=n, density=density, rand.x=rnorm)
+      A[lower.tri(A)] <- t(A)[lower.tri(A)] # Transform A into a symmetric matrix
+      diag(A) <- (diag(A) + 2.0)
+      cat(c("A symmetric: ", all.equal(A, t(A)), "\n"), sep="")
+      print("sym")
+    }
+    else if ("spd" %in% type) {
+      A <- rsparsematrix(nrow=n, ncol=n, density=density)
+      A[lower.tri(A)] <- t(A)[lower.tri(A)] # Transform A into a symmetric matrix
+      diag(A) <- (diag(A) + n)              # Make A Positive Definite
+      cat(c("A symmetric: ", all.equal(A, t(A)), "\n"), sep="")
+      print("spd")
+    }
+    else if ("tri" %in% type) {
+      A <- rsparsematrix(nrow=n, ncol=n, density=2.0*density)
+      A[upper.tri(A, diag=FALSE)] <- 0.0 # Transform A into a lower triangular matrix
+      diag(A) <- (diag(A) + 1.0)
+      print("tri")
+    }
+    print(condest(A)$est)
+    print(nnzero(A))
+
+    B <- matrix(data = rnorm(n * rhs), nrow=n, ncol=rhs)
     ct <- matrix(0.0, ncol=ncol(B), nrow=nrow(B))
     cache_scrub()
     start.time <- Sys.time()
@@ -13,28 +42,10 @@ solve_b <- function(A, B){
 
 properties_solve_sparse <- function(b, n, rhs, density){
 
-    B <- matrix(data = rnorm(n * rhs), nrow=n, ncol=rhs)
-
-    A <- rsparsematrix(n, n, density=density, rand.x=rnorm)
-    diag(A) <- diag(A) + 1.0
-    print(nnzero(A))
-    res1 <- benchmark('solve_sp_gen', solve_b, A, B)
-
-    A <- rsparsematrix(nrow=n, ncol=n, density=density, rand.x=rnorm)
-    A[lower.tri(A)] <- t(A)[lower.tri(A)] # Transform A into a symmetric matrix
-    diag(A) <- (diag(A) + 2.0)
-    print(nnzero(A))
-    cat(c("A symmetric: ", all.equal(A, t(A)), "\n"), sep="")
-    res2 <- benchmark('solve_sp_sym', solve_b, A, B)
-
-    A <- rsparsematrix(nrow=n, ncol=n, density=density)
-    A[lower.tri(A)] <- t(A)[lower.tri(A)] # Transform A into a symmetric matrix
-    diag(A) <- (diag(A) + n)              # Make A Positive Definite
-    cat(c("A symmetric: ", all.equal(A, t(A)), "\n"), sep="")
-    res1 <- benchmark('solve_sp_spd', solve_b, A, B)
-
-    A <- rsparsematrix(nrow=n, ncol=n, density=2.0*density)
-    A[upper.tri(A, diag=FALSE)] <- 0.0 # Transform A into a lower triangular matrix
-    diag(A) <- (diag(A) + 1.0)
-    res3 <- benchmark('solve_sp_tri', solve_b, A, B)
+  # I create the matrices withing the function being benchmarked every time because R caches the results somehow and I
+  # can't find a function that would just perform a "deepcopy" equivalent to Python to the arguments supplied.
+  res1 <- benchmark('solve_sp_gen', solve_b, n, rhs, "gen")
+  res2 <- benchmark('solve_sp_sym', solve_b, n, rhs, "sym")
+  res3 <- benchmark('solve_sp_spd', solve_b, n, rhs, "spd")
+  res4 <- benchmark('solve_sp_tri', solve_b, n, rhs, "tri")
 }
