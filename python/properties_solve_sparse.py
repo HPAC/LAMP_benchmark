40c892f6ce54b5cfea75ff25b2cce91086a4bf83
Christos Psarras <psarras@aices.rwth-aachen.de>
2020-11-26 10:28:13 +0100

    Fix sparse for Python

--- python/properties_solve_sparse.py
+++ python/properties_solve_sparse.py
@@ -1,5 +1,6 @@
 from benchmarker import cache_scrub
 from scipy.sparse.linalg import spsolve
+from scipy import io, sparse
 import scipy.sparse as sp
 import numpy as np
 import logging
@@ -25,17 +26,27 @@ def properties_solve_sparse(b, n, rhs, density):
 
     # General
     A = sp.random(n, n, density=density, format='csc', data_rvs=np.random.randn) + sp.eye(n)
-    logger.info('onenormest: {:.1f}, nnz: {}'.format(sp.linalg.onenormest(A), A.nnz))
+    io.savemat('gen_sparse.mat', {'A':A})
+    logger.info('nnz: {}'.format(A.nnz))
     b.benchmark('solve_sp_gen', my_solve, A, B)
 
     # Symmetric
     A = sp.random(n, n, density=density/2.0, format='csc', data_rvs=np.random.randn)
     A = A + A.T + 2.0 * sp.eye(n)
-    logger.info('onenormest: {:.1f}, nnz: {}'.format(sp.linalg.onenormest(A), A.nnz))
+    io.savemat('sym_sparse.mat', {'As':A})
+    logger.info('nnz: {}'.format(A.nnz))
     b.benchmark('solve_sp_sym', my_solve, A, B)
 
     # SPD
     A = sp.random(n, n, density=density/2.0, format='csc', data_rvs=np.random.randn)
     A = A + A.transpose() + n * sp.eye(n)
-    logger.info('onenormest: {:.1f}, nnz: {}'.format(sp.linalg.onenormest(A), A.nnz))
+    io.savemat('spd_sparse.mat', {'Aspd':A})
+    logger.info('nnz: {}'.format(A.nnz))
     b.benchmark('solve_sp_spd', my_solve, A, B)
+
+    # Triangular
+    A = sp.random(n, n, density=density*2.0, format='csc', data_rvs=np.random.randn)
+    A = sp.tril(A) + sp.eye(n)
+    io.savemat('tri_sparse.mat', {'Atri':A})
+    logger.info('nnz: {}'.format(A.nnz))
+    b.benchmark('solve_sp_tri', my_solve, A, B)
