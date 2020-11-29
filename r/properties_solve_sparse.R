a004dfe3243bd8d4e66cf9d2c21c414504fc39fd
Christos Psarras <psarras@aices.rwth-aachen.de>
2020-11-29 14:20:27 +0100

    Fix sparse experiments in R

--- r/properties_solve_sparse.R
+++ r/properties_solve_sparse.R
@@ -3,41 +3,56 @@ library(Matrix)
 
 solve_b <- function(n, rhs, type) {
 
+    print("selecting matrix")
     if ("gen" %in% type) {
       A <- rsparsematrix(n, n, density=density, rand.x=rnorm)
-      diag(A) <- diag(A) + 1.0
+      diag(A) <- (diag(A) + 1.0)
+      print(isSymmetric(A))
       print("gen")
     }
     else if  ("sym" %in% type) {
-      A <- rsparsematrix(nrow=n, ncol=n, density=density, rand.x=rnorm)
-      A[lower.tri(A)] <- t(A)[lower.tri(A)] # Transform A into a symmetric matrix
+      A <- rsparsematrix(nrow=n, ncol=n, density=density, rand.x=rnorm, symmetric=TRUE)
+      A <- as(A, "sparseMatrix")
       diag(A) <- (diag(A) + 2.0)
-      cat(c("A symmetric: ", all.equal(A, t(A)), "\n"), sep="")
+      print(isSymmetric(A))
       print("sym")
     }
     else if ("spd" %in% type) {
-      A <- rsparsematrix(nrow=n, ncol=n, density=density)
-      A[lower.tri(A)] <- t(A)[lower.tri(A)] # Transform A into a symmetric matrix
-      diag(A) <- (diag(A) + n)              # Make A Positive Definite
-      cat(c("A symmetric: ", all.equal(A, t(A)), "\n"), sep="")
+      A <- rsparsematrix(nrow=n, ncol=n, density=density, rand.x=rnorm, symmetric=TRUE)
+      A <- as(A, "sparseMatrix")
+      diag(A) <- (diag(A) + n)  # Make A Positive Definite
+      print(isSymmetric(A))
       print("spd")
     }
     else if ("tri" %in% type) {
-      A <- rsparsematrix(nrow=n, ncol=n, density=2.0*density)
-      A[upper.tri(A, diag=FALSE)] <- 0.0 # Transform A into a lower triangular matrix
+      A <- rsparsematrix(nrow=n, ncol=n, density=2.0*density, rand.x=rnorm)
+      mat.summ   <- summary(A)
+      lower.summ <- subset(mat.summ, i >= j)
+      print(dim(A))
+      A <- sparseMatrix(i = lower.summ$i, j = lower.summ$j, x = lower.summ$x, dims = dim(A))
+      A <- as(A, "dtCMatrix")
+      A <- as(A, "sparseMatrix")
       diag(A) <- (diag(A) + 1.0)
+      print(isTriangular(A))
       print("tri")
     }
-    print(condest(A)$est)
+    # print(condest(A)$est)  # Comment out because R caches the factorization to use later to solve the linear system.
     print(nnzero(A))
 
     B <- matrix(data = rnorm(n * rhs), nrow=n, ncol=rhs)
     ct <- matrix(0.0, ncol=ncol(B), nrow=nrow(B))
+    cstime_start <- Sys.time()
     cache_scrub()
-    start.time <- Sys.time()
+    cstime_end <- Sys.time()
+    print("cs_time:")
+    print(as.double(cstime_end - cstime_start))
+
+    solve_time_start <- Sys.time()
     ct <- solve(A, B)
-    end.time <- Sys.time()
-    return(tuple(as.double(end.time-start.time), ct))
+    solve_time_end <- Sys.time()
+    print("solve_time")
+    print(as.double(solve_time_end-solve_time_start))
+    return(tuple(as.double(solve_time_end-solve_time_start), ct))
 }
 
 properties_solve_sparse <- function(b, n, rhs, density){
