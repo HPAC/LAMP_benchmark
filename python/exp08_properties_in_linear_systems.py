2ef686f64718abcf042161f303e976465e67086f
Navid-github <navid.alashti@gmail.com>
2021-01-28 21:02:12 +0100

    new update exp08

--- python/exp08_properties_in_linear_systems.py
+++ python/exp08_properties_in_linear_systems.py
@@ -1,23 +1,19 @@
-from scipy import linalg
 import numpy as np
 import logging
-import time
-import copy
-from benchmarker import cache_scrub
 from benchmarker import benchmark
 
-logger = logging.getLogger('PropertiesSolve')
+logger = logging.getLogger('exp08_properties_in_linear_systems')
 
 @benchmark
-def solve(A, B):
-    ct = copy.deepcopy(B)
-    ct = np.linalg.solve(A, B)
-    return ct
+def solve(A, B, C):
+    C = np.linalg.solve(A, B)
+    return C
 
 
 def exp08_properties_in_linear_systems(b, n, rhs):
 
     B = np.random.randn(n, rhs)
+    C = np.zeros((n, rhs), dtype=np.float64)
 
     # SPD
     A = np.random.randn(n, n)
@@ -28,7 +24,7 @@ def exp08_properties_in_linear_systems(b, n, rhs):
         logger.info('A is SPD: True')
     except np.linalg.LinAlgError:
         logger.info('A is SPD: False')
-    b.benchmark('solve_spd', solve, A, B)
+    b.benchmark('solve_spd', solve, A, B, C)
 
     # Symmetric
     A = np.random.randn(n, n)
@@ -40,16 +36,16 @@ def exp08_properties_in_linear_systems(b, n, rhs):
         logger.info('A is SPD: True')
     except np.linalg.LinAlgError:
         logger.info('A is SPD: False')
-    b.benchmark('solve_sym', solve, A, B)
+    b.benchmark('solve_sym', solve, A, B, C)
 
     # Triangular
     A = np.random.randn(n, n)
     A = np.tril(A)
     logger.info('A is triangular: {}'.format(np.allclose(A, np.tril(A))))
-    b.benchmark('solve_tri', solve, A, B)
+    b.benchmark('solve_tri', solve, A, B, C)
 
     # Diagonal
     A = np.random.randn(n, n)
     A = np.diag(np.diag(A))
     logger.info('A is diagonal: {}'.format(np.allclose(A, np.diag(np.diag(A)))))
-    b.benchmark('solve_dia', solve, A, B)
+    b.benchmark('solve_dia', solve, A, B, C)
