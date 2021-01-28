c922fd55a77aeed96c40e5c7937d1779d8660af5
Navid-github <navid.alashti@gmail.com>
2021-01-28 21:13:38 +0100

    new update exp10

--- python/exp10_loop_invariant_code_motion.py
+++ python/exp10_loop_invariant_code_motion.py
@@ -1,9 +1,22 @@
-from scipy import linalg
 import numpy as np
 import logging
 from benchmarker import benchmark
 
-logger = logging.getLogger('IndexProblems')
+logger = logging.getLogger('exp10_loop_invariant_code_motion')
+
+@benchmark
+def naive_loop(A, B, C, temp):
+    for i in range(C.shape[0]):
+        temp = A @ B
+        C[i] = temp[i, i]
+    return C
+
+@benchmark
+def recommended_loop(A, B, C, temp):
+    temp = A @ B
+    for i in range(C.shape[0]):
+        C[i] = temp[i, i]
+    return C
 
 
 def exp10_loop_invariant_code_motion(b, n):
@@ -12,22 +25,8 @@ def exp10_loop_invariant_code_motion(b, n):
     A = np.random.randn(ipn, ipn)
     B = np.random.randn(ipn, ipn)
     C = np.random.randn(ipn)
-
-    res1 = b.benchmark("loop_nai", naive_loop, A, B, C)
-    res2 = b.benchmark("loop_rec", recommended_loop, A, B, C)
-    logger.info('IndexProblems correctness: {}'.format(np.allclose(res1, res2)))
-
-@benchmark
-def naive_loop(A, B, C):
     temp = np.zeros((A.shape[0], B.shape[1]), dtype=np.float64)
-    for i in range(C.shape[0]):
-        temp = A @ B
-        C[i] = temp[i, i]
-    return C
 
-@benchmark
-def recommended_loop(A, B, C):
-    temp = A @ B
-    for i in range(C.shape[0]):
-        C[i] = temp[i, i]
-    return C
+    res1 = b.benchmark("loop_nai", naive_loop, A, B, C, temp)
+    res2 = b.benchmark("loop_rec", recommended_loop, A, B, C, temp)
+    logger.info('IndexProblems correctness: {}'.format(np.allclose(res1, res2)))
