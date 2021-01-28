a95868ebd4031ccd5f5f4941fd5c05b37b61aaf8
Navid-github <navid.alashti@gmail.com>
2021-01-28 21:08:11 +0100

    new update exp09

--- python/exp09_common_subexpressions.py
+++ python/exp09_common_subexpressions.py
@@ -1,17 +1,16 @@
-from scipy import linalg
 import numpy as np
 import logging
 from benchmarker import benchmark
 
-logger = logging.getLogger('CommonSubExpr')
+logger = logging.getLogger('exp09_common_subexpressions')
 
 @benchmark
-def subexpr_nai(A, B):
+def subexpr_nai(A, B, kt):
     kt = A @ B + A @ B
     return kt
 
 @benchmark
-def subexpr_rec(A, B):
+def subexpr_rec(A, B, kt):
     kt = A @ B
     kt = kt + kt
     return kt
@@ -21,7 +20,8 @@ def exp09_common_subexpressions(b, n):
 
     A = np.random.randn(n, n)
     B = np.random.randn(n, n)
+    kt = np.zeros((n, n), dtype=np.float64)
 
-    res1 = b.benchmark('subexpr_nai', subexpr_nai, A, B)
-    res2 = b.benchmark('subexpr_rec', subexpr_rec, A, B)
+    res1 = b.benchmark('subexpr_nai', subexpr_nai, A, B, kt)
+    res2 = b.benchmark('subexpr_rec', subexpr_rec, A, B, kt)
     logger.info('CommonSubExpr correctness: {}'.format(np.allclose(res1, res2)))
