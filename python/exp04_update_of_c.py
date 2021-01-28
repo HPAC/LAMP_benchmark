e9928d5cc72deb7d2b6020ba25be283e17b31a48
Navid-github <navid.alashti@gmail.com>
2021-01-28 20:46:14 +0100

    Moving all contents of `add_scal` file to `exp04_update_of_c`

--- python/exp04_update_of_c.py
+++ python/exp04_update_of_c.py
@@ -1,5 +1,26 @@
-from benchmarker import benchmark
 import logging
 import numpy as np
 from scipy import linalg
+from benchmarker import benchmark
 
+logger = logging.getLogger('exp04_update_of_c')
+
+@benchmark
+def add(A, B):
+    B = A + B
+    return B
+
+@benchmark
+def scal(A):
+    A = 3.0 * A
+    return A
+
+
+
+def exp04_update_of_c(b, n):
+
+    A = np.random.randn(n, n)
+    B = np.random.randn(n, n)
+
+    b.benchmark('add', add, A, B)
+    b.benchmark('scal', scal, A)
--- python/main.py
+++ python/main.py
@@ -8,7 +8,7 @@ from benchmarker import Benchmarker as ben
 from exp01_gemm import exp01_gemm
 from exp02_syrk import exp02_syrk
 from exp03_syr2k import exp03_syr2k
-# from exp04_update_of_c import exp04_update_of_c
+from exp04_update_of_c import exp04_update_of_c
 from exp05_explicit_inversion import exp05_explicit_inversion
 from exp06_optimal_parenthesization import exp06_optimal_parenthesization
 from exp07_properties_multiplication import exp07_properties_multiplication
@@ -17,11 +17,8 @@ from exp09_common_subexpressions import exp09_common_subexpressions
 from exp10_loop_invariant_code_motion import exp10_loop_invariant_code_motion
 from exp11_blocked_matrices import exp11_blocked_matrices
 from exp12_partial_operand_access import exp12_partial_operand_access
-from expNN_transposition import expNN_transposition
-
 from expNN_BLAS_level_2_to_level_3 import expNN_BLAS_level_2_to_level_3
-from add_scal import add_scal
-
+from expNN_transposition import expNN_transposition
 
 import logging
 
@@ -34,43 +31,52 @@ n = int(os.environ['LAMP_N'])
 
 b = ben('python_' + str(os.environ['OMP_NUM_THREADS']))
 
-# ! GEMM
+
 exp01_gemm(b, n)
 
-# add_scal
-A = np.random.randn(n, n)
-B = np.random.randn(n, n)
-add_scal(b, A, B)
-
-# ! Properties Solve
-exp08_properties_in_linear_systems(b, n, 200)
-
-# ! SYRK
 exp02_syrk(b, n)
 
-# ! SYR2K
 exp03_syr2k(b, n)
 
-# ! Transposition
-expNN_transposition(b, n)
+exp04_update_of_c(b, n)
 
-# ! Common Subexpression
-exp09_common_subexpressions(b, n)
-
-# ! Composed Operations
 exp05_explicit_inversion(b, n)
 
-#! Matrix Chain Problem
 exp06_optimal_parenthesization(b, n)
 
-# ! Partial Operand
-exp12_partial_operand_access(b, n)
+exp07_properties_multiplication(b, n)
 
-# ! Index problems
+exp08_properties_in_linear_systems(b, n, 200)
+
+exp09_common_subexpressions(b, n)
+
 exp10_loop_invariant_code_motion(b, n)
 
-# ! Index problems
+exp11_blocked_matrices(b, n)
+
+exp12_partial_operand_access(b, n)
+
 expNN_BLAS_level_2_to_level_3(b, n)
 
-# ! Partitioned Matrices
-exp11_blocked_matrices(b, n)
+expNN_transposition(b, n)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
