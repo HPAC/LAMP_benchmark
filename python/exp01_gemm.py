ae97e63cde8b019a9d877438a5c1147178a81684
Navid-github <navid.alashti@gmail.com>
2021-02-06 12:35:30 +0100

    Move `diagmm` from `exp01_gemm.py` to `exp07_properties_multiplication.py`

--- python/exp01_gemm.py
+++ python/exp01_gemm.py
@@ -1,17 +1,11 @@
 from benchmarker import benchmark
 import logging
 import numpy as np
-from scipy import linalg
 
 logger = logging.getLogger('exp01_gemm')
 
 @benchmark
 def gemm_implicit_noup(A, B, C):
-    C = A @ B
-    return C
-
-@benchmark
-def diagmm(A, B, C):
     C = A @ B
     return C
 
@@ -22,17 +16,9 @@ def exp01_gemm(b, n):
     B = np.random.randn(n, n)
     C = np.random.randn(n, n)
 
-    res1 = b.benchmark('gemm_implicit_noup', gemm_implicit_noup, A, B, C)
+    res = b.benchmark('gemm_implicit_noup', gemm_implicit_noup, A, B, C)
 
-    Asq = np.random.randn(A.shape[0], A.shape[0])
-    Bsq = np.random.randn(A.shape[0], A.shape[0])
-    Csq = np.random.randn(A.shape[0], A.shape[0])
 
-    Asq = np.tril(Asq)
-    logger.debug('Asq is triangualar'.format(np.allclose(Asq, np.tril(Asq))))
-
-    Ad = np.diag(np.diag(Asq))
-    res2 = b.benchmark('diagmm', diagmm, Ad, Bsq, Csq)
 
 
 
--- python/exp07_properties_multiplication.py
+++ python/exp07_properties_multiplication.py
@@ -10,22 +10,30 @@ def trmm_implicit(A, B):
     B = A @ B
     return B
 
-
 @benchmark
 def trmm_explicit(A, B):
     linalg.blas.dtrmm(1.0, A, B, diag=False, trans_a=False, side=False,
                       lower=True, overwrite_b=True)  # diag specifies unit triangular
     return B
 
+@benchmark
+def diagmm(A, B, C):
+    C = A @ B
+    return C
+
 
 def exp07_properties_multiplication(b, n):
 
-    Asq = np.random.randn(n, n)
-    Bsq = np.random.randn(n, n)
+    A = np.random.randn(n, n)
+    B = np.random.randn(n, n)
+    C = np.random.randn(n, n)
 
-    Asq = np.tril(Asq)
-    logger.debug('Asq is triangualar'.format(np.allclose(Asq, np.tril(Asq))))
-    res7 = b.benchmark('trmm_implicit', trmm_implicit, Asq, Bsq)
-    res8 = b.benchmark('trmm_explicit', trmm_explicit, Asq, Bsq)
+    A = np.tril(A)
+    logger.debug('A is triangualar'.format(np.allclose(A, np.tril(A))))
+    res1 = b.benchmark('trmm_implicit', trmm_implicit, A, B)
+    res2 = b.benchmark('trmm_explicit', trmm_explicit, A, B)
 
-    logger.info('Trmm correctness: {}'.format(np.allclose(res7, res8)))
+    Ad = np.diag(np.diag(A))
+    res3 = b.benchmark('diagmm', diagmm, Ad, B, C)
+
+    logger.info('Trmm correctness: {}'.format(np.allclose(res1, res2)))
