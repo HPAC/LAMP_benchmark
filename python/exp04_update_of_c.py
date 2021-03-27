64afccb03dd558b03892320f05b7dd0d68bafdea
Navid-github <navid.alashti@gmail.com>
2021-03-27 22:59:50 +0100

    updating exp04

--- python/exp04_update_of_c.py
+++ python/exp04_update_of_c.py
@@ -1,7 +1,7 @@
 import logging
-import numpy as np
-from scipy import linalg
 from benchmarker import benchmark
+import tensorflow as tf
+import numpy as np
 
 logger = logging.getLogger('exp04_update_of_c')
 
@@ -37,80 +37,57 @@ def gemm_implicit_double_coeff(A, B, C):
     C = 3.0 * A @ B + 3.0 * C
     return C
 
-@benchmark
-def gemm_explicit(A, B, C):
-    linalg.blas.dgemm(1.0, A, B, 1.0, C, trans_a=False, trans_b=False, overwrite_c=True)
-    return C
-
 # from exp02_syrk
 
 @benchmark
 def syrk_implicit(A, C):
-    C = A @ A.T + C
+    C = A @ tf.transpose(A) + C
     return C
 
 @benchmark
 def syrk_implicit_compact(A, C):
-    C += A @ A.T
-    return C
-
-@benchmark
-def syrk_explicit(A, C):
-    linalg.blas.dsyrk(1.0, A, 1.0, C, overwrite_c=True, lower=True, trans=False)
+    C += A @ tf.transpose(A)
     return C
 
 # from exp03_syr2k
 
 @benchmark
 def syr2k_implicit(A, B, C):
-    C = A @ B.T + B @ A.T + C
+    C = A @ tf.transpose(B) + B @ tf.transpose(A) + C
     return C
 
 @benchmark
 def syr2k_implicit_compact(A, B, C):
-    C += A @ B.T + B @ A.T
-    return C
-
-@benchmark
-def syr2k_explicit(A, B, C):
-    linalg.blas.dsyr2k(1.0, A, B, 1.0, C, overwrite_c=True, lower=True, trans=False)
+    C += A @ tf.transpose(B) + B @ tf.transpose(A)
     return C
 
 def exp04_update_of_c(b, n):
 
-    A = np.random.randn(n, n)
-    B = np.random.randn(n, n)
-    C = np.random.randn(n, n)
-    C_fortran_style = C.ravel(order='F').reshape(C.shape, order='F')
+    A = tf.random.normal([n, n])
+    B = tf.random.normal([n, n])
+    C = tf.random.normal([n, n])
 
     b.benchmark('add', add, A, B)
     b.benchmark('scal', scal, A)
 
     # from exp01_gemm
-    res01 = b.benchmark('gemm_implicit', gemm_implicit, A, B, C)
-    res02 = b.benchmark('gemm_implicit_compact', gemm_implicit_compact, A, B, C)
-    res03 = b.benchmark('gemm_explicit', gemm_explicit, A, B, C_fortran_style)
-    res04 = b.benchmark('gemm_implicit_coeff', gemm_implicit_coeff, A, B, C)
-    res05 = b.benchmark('gemm_implicit_double_coeff', gemm_implicit_double_coeff, A, B, C)
+    res1 = b.benchmark('gemm_implicit', gemm_implicit, A, B, C)
+    res2 = b.benchmark('gemm_implicit_compact', gemm_implicit_compact, A, B, C)
+    res3 = b.benchmark('gemm_implicit_coeff', gemm_implicit_coeff, A, B, C)
+    res4 = b.benchmark('gemm_implicit_double_coeff', gemm_implicit_double_coeff, A, B, C)
 
-    logger.info('Gemm correctness: {}'.format(np.allclose(res01, res02)))
-    logger.info('Gemm correctness: {}'.format(np.allclose(res01, res03)))
+    logger.info('Gemm correctness: {}'.format(tf.experimental.numpy.allclose(res1, res2)))
 
     # from exp02_syrk
-    C = C + C.T
-    C_fortran_style = C.ravel(order='F').reshape(C.shape, order='F')
+    C = C + tf.transpose(C)
 
-    res06 = b.benchmark('syrk_implicit', syrk_implicit, A, C)
-    res07 = b.benchmark('syrk_implicit_compact', syrk_implicit_compact, A, C)
-    res08 = b.benchmark('syrk_explicit', syrk_explicit, A, C_fortran_style)
+    res5 = b.benchmark('syrk_implicit', syrk_implicit, A, C)
+    res6 = b.benchmark('syrk_implicit_compact', syrk_implicit_compact, A, C)
 
-    logger.info('Syrk correctness: {}'.format(np.allclose(np.tril(res06, k=0), np.tril(res07, k=0))))
-    logger.info('Syrk correctness: {}'.format(np.allclose(np.tril(res06, k=0), np.tril(res08, k=0))))
+    logger.info('Syrk correctness: {}'.format(np.allclose(tf.experimental.numpy.tril(res5, k=0), tf.experimental.numpy.tril(res6, k=0))))
 
     # from exp03_syr2k
-    res09 = b.benchmark('syr2k_implicit', syr2k_implicit, A, B, C)
-    res10 = b.benchmark('syr2k_implicit_compact', syr2k_implicit_compact, A, B, C)
-    res11 = b.benchmark('syr2k_explicit', syr2k_explicit, A, B, C_fortran_style)
+    res7 = b.benchmark('syr2k_implicit', syr2k_implicit, A, B, C)
+    res8 = b.benchmark('syr2k_implicit_compact', syr2k_implicit_compact, A, B, C)
 
-    logger.info('Syr2k correctness: {}'.format(np.allclose(np.tril(res09, k=0), np.tril(res10, k=0))))
-    logger.info('Syr2k correctness: {}'.format(np.allclose(np.tril(res09, k=0), np.tril(res11, k=0))))
+    logger.info('Syr2k correctness: {}'.format(np.allclose(tf.experimental.numpy.tril(res7, k=0), tf.experimental.numpy.tril(res8, k=0))))
