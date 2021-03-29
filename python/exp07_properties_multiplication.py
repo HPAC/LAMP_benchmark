dfb7c545e80fae7a6c9044a93d89d825ca3555b8
Navid-github <navid.alashti@gmail.com>
2021-03-29 20:04:48 +0200

    first update (exp07)

--- python/exp07_properties_multiplication.py
+++ python/exp07_properties_multiplication.py
@@ -1,19 +1,12 @@
 from benchmarker import benchmark
 import logging
-import numpy as np
-from scipy import linalg
+import torch as torch
 
 logger = logging.getLogger('exp07_properties_multiplication')
 
 @benchmark
 def trmm_implicit(A, B):
     B = A @ B
-    return B
-
-@benchmark
-def trmm_explicit(A, B):
-    linalg.blas.dtrmm(1.0, A, B, diag=False, trans_a=False, side=False,
-                      lower=True, overwrite_b=True)  # diag specifies unit triangular
     return B
 
 @benchmark
@@ -24,17 +17,13 @@ def diagmm(A, B, C):
 
 def exp07_properties_multiplication(b, n):
 
-    A = np.random.randn(n, n)
-    B = np.random.randn(n, n)
-    C = np.random.randn(n, n)
-    B_fortran_style = B.ravel(order='F').reshape(B.shape, order='F')
+    A = torch.randn(n, n)
+    B = torch.randn(n, n)
+    C = torch.randn(n, n)
 
-    A = np.tril(A)
-    logger.debug('A is triangualar'.format(np.allclose(A, np.tril(A))))
+    A = torch.tril(A)
+    logger.debug('A is triangualar'.format(torch.allclose(A, torch.tril(A))))
     res1 = b.benchmark('trmm_implicit', trmm_implicit, A, B)
-    res2 = b.benchmark('trmm_explicit', trmm_explicit, A, B_fortran_style)
 
-    Ad = np.diag(np.diag(A))
-    res3 = b.benchmark('diagmm', diagmm, Ad, B, C)
-
-    logger.info('Trmm correctness: {}'.format(np.allclose(res1, res2)))
+    Ad = torch.diag(torch.diag(A))
+    res2 = b.benchmark('diagmm', diagmm, Ad, B, C)
