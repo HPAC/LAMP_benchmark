3dbc4b792d98113adcf34076c49a3a078683d7b0
Navid-github <navid.alashti@gmail.com>
2021-04-04 09:58:36 +0200

    first update (exp08)

--- python/exp08_properties_in_linear_systems.py
+++ python/exp08_properties_in_linear_systems.py
@@ -1,51 +1,52 @@
-import numpy as np
 import logging
 from benchmarker import benchmark
+import torch as torch
+import numpy as np
 
 logger = logging.getLogger('exp08_properties_in_linear_systems')
 
 @benchmark
 def solve(A, B, C):
-    C = np.linalg.solve(A, B)
+    C = torch.linalg.solve(A, B)
     return C
 
 
 def exp08_properties_in_linear_systems(b, n, rhs):
 
-    B = np.random.randn(n, rhs)
-    C = np.zeros((n, rhs), dtype=np.float64)
+    B = torch.randn(n, rhs)
+    C = torch.zeros((n, rhs), dtype=torch.float64)
 
     # SPD
-    A = np.random.randn(n, n)
-    A = A + A.T + np.eye(n, dtype=np.float64) * n
-    logger.info('A is symmetric: {}'.format(np.allclose(A, A.T)))
+    A = torch.randn(n, n)
+    A = A + torch.transpose(A, 0, 1) + torch.eye(n, dtype=torch.float64) * n
+    logger.info('A is symmetric: {}'.format(torch.allclose(A, torch.transpose(A, 0, 1))))
     try:
-        np.linalg.cholesky(A)
+        torch.linalg.cholesky(A)
         logger.info('A is SPD: True')
     except np.linalg.LinAlgError:
         logger.info('A is SPD: False')
     b.benchmark('solve_spd', solve, A, B, C)
 
     # Symmetric
-    A = np.random.randn(n, n)
-    A = A + A.T
+    A = torch.randn(n, n)
+    A = A + torch.transpose(A, 0, 1)
     A[1, 1] = -1.0
-    logger.info('A is symmetric: {}'.format(np.allclose(A, A.T)))
+    logger.info('A is symmetric: {}'.format(torch.allclose(A, torch.transpose(A, 0, 1))))
     try:
-        np.linalg.cholesky(A)
+        torch.linalg.cholesky(A)
         logger.info('A is SPD: True')
     except np.linalg.LinAlgError:
         logger.info('A is SPD: False')
     b.benchmark('solve_sym', solve, A, B, C)
 
     # Triangular
-    A = np.random.randn(n, n)
-    A = np.tril(A)
-    logger.info('A is triangular: {}'.format(np.allclose(A, np.tril(A))))
+    A = torch.randn(n, n)
+    A = torch.tril(A)
+    logger.info('A is triangular: {}'.format(torch.allclose(A, torch.tril(A))))
     b.benchmark('solve_tri', solve, A, B, C)
 
     # Diagonal
-    A = np.random.randn(n, n)
-    A = np.diag(np.diag(A))
-    logger.info('A is diagonal: {}'.format(np.allclose(A, np.diag(np.diag(A)))))
+    A = torch.randn(n, n)
+    A = torch.diag(torch.diag(A))
+    logger.info('A is diagonal: {}'.format(torch.allclose(A, torch.diag(torch.diag(A)))))
     b.benchmark('solve_dia', solve, A, B, C)
