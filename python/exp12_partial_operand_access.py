272832978ebe00cb43eb6932aac554c34b3f6580
Navid-github <navid.alashti@gmail.com>
2021-04-04 10:47:54 +0200

    first update (exp11 and exp12)

--- python/exp12_partial_operand_access.py
+++ python/exp12_partial_operand_access.py
@@ -1,7 +1,7 @@
 import copy
-import numpy as np
 import logging
 from benchmarker import benchmark
+import torch as torch
 
 logger = logging.getLogger('exp12_partial_operand_access')
 
@@ -37,40 +37,40 @@ def par_op_col_add_rec(A, B, C):
 
 @benchmark
 def diagonal_mult_nai(A, B, C):
-    C = np.diagonal(A @ B)
+    C = torch.diagonal(A @ B)
     return C
 
 @benchmark
 def diagonal_nai(A, B, C):
-    C = np.diagonal(A + B)
+    C = torch.diagonal(A + B)
     return C
 
 @benchmark
 def diagonal_rec(A, B, C):
-    C = np.diagonal(A) + np.diagonal(B)
+    C = torch.diagonal(A) + torch.diagonal(B)
     return C
 
 
 def exp12_partial_operand_access(b, n):
 
-    A = np.random.randn(n, n)
-    B = np.random.randn(n, n)
-    C = np.random.randn(n)
+    A = torch.randn(n, n)
+    B = torch.randn(n, n)
+    C = torch.randn(n)
 
     res3 = b.benchmark('part_op_col_mult_nai', par_op_col_mult_nai,  A, B, C)
     res1 = b.benchmark('part_op_col_add_nai', par_op_col_add_nai,  A, B, C)
     res2 = b.benchmark('part_op_col_add_rec', par_op_col_add_rec,  A, B, C)
-    logger.info('PartialOperand correctness: {}'.format(np.allclose(res1, res2)))
+    logger.info('PartialOperand correctness: {}'.format(torch.allclose(res1, res2)))
 
-    C = np.random.randn(1)
+    C = torch.randn(1)
     res6 = b.benchmark('part_op_ele_mult_nai', par_op_ele_mult_nai,  A, B, C)
     res4 = b.benchmark('part_op_ele_add_nai', par_op_ele_add_nai,  A, B, C)
     res5 = b.benchmark('part_op_ele_add_rec', par_op_ele_add_rec,  A, B, C)
-    logger.info('PartialOperand correctness: {}'.format(np.allclose(res4, res5)))
+    logger.info('PartialOperand correctness: {}'.format(torch.allclose(res4, res5)))
 
     C = copy.deepcopy(B)
     C = C.diagonal()
     res9 = b.benchmark('diag_mult_nai', diagonal_mult_nai, A, B, C)
     res7 = b.benchmark('diag_add_nai', diagonal_nai, A, B, C)
     res8 = b.benchmark('diag_add_rec', diagonal_rec, A, B, C)
-    logger.info('PartialOperand correctness: {}'.format(np.allclose(res7, res8)))
+    logger.info('PartialOperand correctness: {}'.format(torch.allclose(res7, res8)))
--- python/main.py
+++ python/main.py
@@ -49,11 +49,11 @@ exp08_properties_in_linear_systems(b, n, 200)
 exp09_common_subexpressions(b, n)
 
 exp10_loop_invariant_code_motion(b, n)
-"""
+
 exp11_blocked_matrices(b, n)
 """
 exp12_partial_operand_access(b, n)
-
+"""
 expNN_BLAS_level_2_to_level_3(b, n)
 
 expNN_transposition(b, n)
