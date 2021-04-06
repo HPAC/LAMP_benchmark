0a72db6fd75f00e19d39aa7f8d944f371dec6da8
Navid-github <navid.alashti@gmail.com>
2021-04-06 11:12:43 +0200

    first update (exp08 and exp10)

--- python/exp08_properties_in_linear_systems.py
+++ python/exp08_properties_in_linear_systems.py
@@ -1,51 +1,53 @@
-import numpy as np
 import logging
 from benchmarker import benchmark
+import numpy as np
+import tensorflow as tf
 
 logger = logging.getLogger('exp08_properties_in_linear_systems')
 
 @benchmark
 def solve(A, B, C):
-    C = np.linalg.solve(A, B)
+    C = tf.linalg.solve(A, B)
     return C
 
 
 def exp08_properties_in_linear_systems(b, n, rhs):
 
-    B = np.random.randn(n, rhs)
-    C = np.zeros((n, rhs), dtype=np.float64)
+    B = tf.random.normal([n, rhs], dtype=tf.float64)
+    C = tf.zeros((n, rhs), dtype=tf.float64)
 
     # SPD
-    A = np.random.randn(n, n)
-    A = A + A.T + np.eye(n, dtype=np.float64) * n
-    logger.info('A is symmetric: {}'.format(np.allclose(A, A.T)))
+    A = tf.random.normal([n, n], dtype=tf.float64)
+    A = A + tf.transpose(A) + tf.eye(n, dtype=tf.float64) * n
+    logger.info('A is symmetric: {}'.format(tf.experimental.numpy.allclose(A, tf.transpose(A))))
     try:
-        np.linalg.cholesky(A)
+        tf.linalg.cholesky(A)
         logger.info('A is SPD: True')
     except np.linalg.LinAlgError:
         logger.info('A is SPD: False')
     b.benchmark('solve_spd', solve, A, B, C)
 
     # Symmetric
-    A = np.random.randn(n, n)
-    A = A + A.T
-    A[1, 1] = -1.0
-    logger.info('A is symmetric: {}'.format(np.allclose(A, A.T)))
+    A = tf.random.normal([n, n], dtype=tf.float64)
+    A = A + tf.transpose(A)
+    A = tf.scatter_nd_update(A, [[1, 1]], [-1.0])
+    #A[1, 1] = -1.0
+    logger.info('A is symmetric: {}'.format(tf.experimental.numpy.allclose(A, tf.transpose(A))))
     try:
-        np.linalg.cholesky(A)
+        tf.linalg.cholesky(A)
         logger.info('A is SPD: True')
     except np.linalg.LinAlgError:
         logger.info('A is SPD: False')
     b.benchmark('solve_sym', solve, A, B, C)
 
     # Triangular
-    A = np.random.randn(n, n)
-    A = np.tril(A)
-    logger.info('A is triangular: {}'.format(np.allclose(A, np.tril(A))))
+    A = tf.random.normal([n, n], dtype=tf.float64)
+    A = tf.experimental.numpy.tril(A)
+    logger.info('A is triangular: {}'.format(tf.experimental.numpy.allclose(A, tf.experimental.numpy.tril(A))))
     b.benchmark('solve_tri', solve, A, B, C)
 
     # Diagonal
-    A = np.random.randn(n, n)
-    A = np.diag(np.diag(A))
-    logger.info('A is diagonal: {}'.format(np.allclose(A, np.diag(np.diag(A)))))
+    A = tf.random.normal([n, n], dtype=tf.float64)
+    A = tf.experimental.numpy.diag(tf.experimental.numpy.diag(A))
+    logger.info('A is diagonal: {}'.format(tf.experimental.numpy.allclose(A, tf.experimental.numpy.diag(tf.experimental.numpy.diag(A)))))
     b.benchmark('solve_dia', solve, A, B, C)
--- python/exp10_loop_invariant_code_motion.py
+++ python/exp10_loop_invariant_code_motion.py
@@ -1,6 +1,6 @@
-import numpy as np
 import logging
 from benchmarker import benchmark
+import tensorflow as tf
 
 logger = logging.getLogger('exp10_loop_invariant_code_motion')
 
@@ -8,25 +8,27 @@ logger = logging.getLogger('exp10_loop_invariant_code_motion')
 def naive_loop(A, B, C, temp):
     for i in range(C.shape[0]):
         temp = A @ B
-        C[i] = temp[i, i]
+        C = tf.scatter_nd_update(C, [[i]], [temp[i, i]])
+        #C[i] = temp[i, i]
     return C
 
 @benchmark
 def recommended_loop(A, B, C, temp):
     temp = A @ B
     for i in range(C.shape[0]):
-        C[i] = temp[i, i]
+        C = tf.scatter_nd_update(C, [[i]], [temp[i, i]])
+        #C[i] = temp[i, i]
     return C
 
 
 def exp10_loop_invariant_code_motion(b, n):
 
     licm_n = int(n / 10)
-    A = np.random.randn(licm_n, licm_n)
-    B = np.random.randn(licm_n, licm_n)
-    C = np.random.randn(licm_n)
-    temp = np.zeros((licm_n, licm_n), dtype=np.float64)
+    A = tf.random.normal([licm_n, licm_n], dtype=tf.float64)
+    B = tf.random.normal([licm_n, licm_n], dtype=tf.float64)
+    C = tf.random.normal([licm_n], dtype=tf.float64)
+    temp = tf.zeros((licm_n, licm_n), dtype=tf.float64)
 
     res1 = b.benchmark("loop_nai", naive_loop, A, B, C, temp)
     res2 = b.benchmark("loop_rec", recommended_loop, A, B, C, temp)
-    logger.info('IndexProblems correctness: {}'.format(np.allclose(res1, res2)))
+    logger.info('IndexProblems correctness: {}'.format(tf.experimental.numpy.allclose(res1, res2)))
