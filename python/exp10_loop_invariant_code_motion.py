581e6205087472463032657a65dede56845e7c01
Navid-github <navid.alashti@gmail.com>
2021-04-20 10:00:29 +0200

    lower case for vectors and scalars

--- python/exp10_loop_invariant_code_motion.py
+++ python/exp10_loop_invariant_code_motion.py
@@ -5,18 +5,18 @@ import tensorflow as tf
 logger = logging.getLogger('exp10_loop_invariant_code_motion')
 
 @benchmark
-def naive_loop(A, B, C, temp):
-    for i in range(C.shape[0]):
+def naive_loop(A, B, c, temp):
+    for i in range(c.shape[0]):
         temp = A @ B
-        C = tf.tensor_scatter_nd_update(C, [[i]], [temp[i, i]])
-    return C
+        c = tf.tensor_scatter_nd_update(c, [[i]], [temp[i, i]])
+    return c
 
 @benchmark
-def recommended_loop(A, B, C, temp):
+def recommended_loop(A, B, c, temp):
     temp = A @ B
-    for i in range(C.shape[0]):
-        C = tf.tensor_scatter_nd_update(C, [[i]], [temp[i, i]])
-    return C
+    for i in range(c.shape[0]):
+        c = tf.tensor_scatter_nd_update(c, [[i]], [temp[i, i]])
+    return c
 
 
 def exp10_loop_invariant_code_motion(b, n):
@@ -24,9 +24,9 @@ def exp10_loop_invariant_code_motion(b, n):
     licm_n = int(n / 10)
     A = tf.random.normal([licm_n, licm_n], dtype=tf.float64)
     B = tf.random.normal([licm_n, licm_n], dtype=tf.float64)
-    C = tf.random.normal([licm_n], dtype=tf.float64)
+    c = tf.random.normal([licm_n], dtype=tf.float64)
     temp = tf.zeros((licm_n, licm_n), dtype=tf.float64)
 
-    res1 = b.benchmark("loop_nai", naive_loop, A, B, C, temp)
-    res2 = b.benchmark("loop_rec", recommended_loop, A, B, C, temp)
+    res1 = b.benchmark("loop_nai", naive_loop, A, B, c, temp)
+    res2 = b.benchmark("loop_rec", recommended_loop, A, B, c, temp)
     logger.info('LICM correctness: {}'.format(tf.debugging.assert_near(res1, res2, rtol=1e-05, atol=1e-08)))
