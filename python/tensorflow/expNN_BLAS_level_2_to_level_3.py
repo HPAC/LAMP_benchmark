3fc1f1f2f6696ec6b137d53194b2ac409f11ed03
Navid-github <navid.alashti@gmail.com>
2021-04-25 18:36:03 +0200

    minor updates

--- python/tensorflow/expNN_BLAS_level_2_to_level_3.py
+++ python/tensorflow/expNN_BLAS_level_2_to_level_3.py
@@ -1,14 +1,16 @@
 import logging
 from benchmarker import benchmark
-import tensorflow as tf
+import numpy as np
 
 logger = logging.getLogger('expNN_BLAS_level_2_to_level_3')
 
+
 @benchmark
 def naive_loop(A, B, C):
-    #for i in range(C.shape[1]):
-    #    C[:, i] = A @ B[:, i]
+    for i in range(C.shape[1]):
+        C[:, i] = A @ B[:, i]
     return C
+
 
 @benchmark
 def recommended_loop(A, B, C):
@@ -17,10 +19,10 @@ def recommended_loop(A, B, C):
 
 
 def expNN_BLAS_level_2_to_level_3(b, n):
-    A = tf.random.normal([n, n], dtype=tf.float64)
-    B = tf.random.normal([n, n], dtype=tf.float64)
-    C = tf.random.normal([n, n], dtype=tf.float64)
+    A = np.random.randn(n, n)
+    B = np.random.randn(n, n)
+    C = np.random.randn(n, n)
 
     res1 = b.benchmark("loop_translation_nai", naive_loop, A, B, C)
     res2 = b.benchmark("loop_translation_rec", recommended_loop, A, B, C)
-    #logger.info('LoopTranslation correctness: {}'.format(tf.debugging.assert_near(res1, res2, rtol=1e-05, atol=1e-08)))
+    logger.info('LoopTranslation correctness: {}'.format(np.allclose(res1, res2)))
