1377139f468c82f34e6d6b2d51f3efd445a0d387
Christos Psarras <psarras@aices.rwth-aachen.de>
2021-05-04 17:06:11 +0200

    Adjust python script for new libraries

--- scripts/cluster/python.sh
+++ scripts/cluster/python.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 source ${HOME}/.zshrc.lamp
-cd ${LAMP_EXPERIMENTS}/python
 
 echo "-----------------------------"
 echo "$1 Threads"
@@ -13,4 +12,9 @@ module purge
 module load DEVELOP
 module load intel/19.0
 
+cd ${LAMP_EXPERIMENTS}/python/numpy
+${PYTHON} main.py
+cd ${LAMP_EXPERIMENTS}/python/torch
+${PYTHON} main.py
+cd ${LAMP_EXPERIMENTS}/python/tensorflow
 ${PYTHON} main.py
